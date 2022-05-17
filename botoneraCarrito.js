ea07795e696a6e794a9afff11414eb370c4eef5f
slekram <maxi.markels@gmail.com>
2022-05-17 15:57:49 -0300

    agregue los productos y el carrito en claves en el localstorage para simular mejor, en un primer momento el local storage se llena con los datos importados de la base de datos JSON de productos, a partir de ahi se carga siempre del local storage

--- botoneraCarrito.js
+++ botoneraCarrito.js
@@ -1,15 +1,6 @@
 import { carrito } from "./carritoIndex.js";
-import { btnEliminar } from "./carritoIndex.js";
 
-export const eliminarCarrito = (eliminarId) => {
-    // let idEliminar = carrito.find(producto => producto.id === eliminarId);
-    // carrito.splice(idEliminar, 1);
-    // console.log(carrito);
-    let btnEliminar = document.getElementById(`eliminar${eliminarId}`);
-    btnEliminar.addEventListener("click", () =>{
-        btnEliminar.parentElement.remove();
-    })
-}
+
 
 const restarCarrito = () => {
     
